4302684cd64e1b14912ece1bebe11fbf42c494a5
John Spounias <d3cline@pyl0n.com>
2019-05-10 06:44:38 -0600

    wordpress installer alpha

--- core/wordpress/install.sh
+++ core/wordpress/install.sh
@@ -11,28 +11,27 @@ CWHITE2='\033[1;97m'      # White
 CEND='\033[0m'       # Text Reset
 
 # i is for UUID, t is for user token, n is for app name
-while getopts i:t:l:n: option
+while getopts i:n: option
 do
 case "${option}"
 in
 i) UUID=${OPTARG};;
-t) TOKEN=${OPTARG};;
 n) APPNAME=$OPTARG;;
 esac
 done
 
-if [ -z $UUID ] || [ -z $TOKEN ] || [ -z $APPNAME ]
+if [ -z $UUID ] || [ -z $OPAL_TOKEN ] || [ -z $APPNAME ]
 then
      printf $CRED2
      echo 'This command requires the following parameters to function, 
      -i App UUID, used to make API calls to control panel.
-     -t Control panel TOKEN, used to authenticate to the API. 
      -n Application NAME, must match the name in the control panel
+      {$OPAL_TOKEN} Control panel token, used to authenticate to the API. 
      '
      exit 1
 else    
     # Get the server's UUID and verify the app exists, and thus the file schema exists.
-    if serverjson=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $TOKEN"  http://127.0.0.1:8000/api/v0/app/read/$UUID` ;then
+    if serverjson=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN"  https://my.opalstack.com/api/v0/app/read/$UUID` ;then
          printf $CGREEN2
          echo 'UUID validation and server lookup OK.'
          printf $CEND
@@ -45,7 +44,7 @@ else
     
     # create database
     dbsend='{"name": "'"$APPNAME"'", "server": "'"$serverid"'" }'
-    if dbjson=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $TOKEN" -d"$dbsend"  http://127.0.0.1:8000/api/v0/mariadb/autoadd/` ;then
+    if dbjson=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN" -d"$dbsend"  https://my.opalstack.com/api/v0/mariadb/autoadd/` ;then
          export $(echo $dbjson| jq -r '@sh "DBNAME=\(.name) CHARSET=\(.charset) DBID=\(.id) DBUSERID=\(.dbuserid) DBUSER=\(.dbuser) DBPWD=\(.default_password) SERVER=\(.server)"' )
          printf $CGREEN2
          echo 'DB creation OK.'
@@ -65,11 +64,11 @@ else
     echo $DBUSER
     echo $DBPWD
 
-    echo"waiting for 30 seconds so the DB and DBUser can be created"
+    echo "waiting for 30 seconds so the DB and DBUser can be created"
     sleep 30
     
     # check if the DB has been installed, initial request. ------------------------------------------------------------------------------------------------------------
-    if DBOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $TOKEN"  http://127.0.0.1:8000/api/v0/mariadb/read/$DBID` ;then
+    if DBOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN"  https://my.opalstack.com/api/v0/mariadb/read/$DBID` ;then
          printf $CGREEN2
          echo 'DB OK lookup.'
          printf $CEND
@@ -86,7 +85,7 @@ else
     echo $DBOK
 
     sleep 10
-    if DBOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $TOKEN"  http://127.0.0.1:8000/api/v0/mariadb/read/$DBID` ;then
+    if DBOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN"  https://my.opalstack.com/api/v0/mariadb/read/$DBID` ;then
          printf $CGREEN2
          echo 'DB OK lookup.'
          printf $CEND
@@ -98,7 +97,7 @@ else
     done
     
     # check if the DB USER has been installed, initial request. ------------------------------------------------------------------------------------------------------------
-    if DBUOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $TOKEN"  http://127.0.0.1:8000/api/v0/mariauser/read/$DBUSERID` ;then
+    if DBUOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN"  https://my.opalstack.com/api/v0/mariauser/read/$DBUSERID` ;then
          printf $CGREEN2
          echo 'DBUser OK lookup.'
          printf $CEND
@@ -115,7 +114,7 @@ else
     echo $DBUOK
 
     sleep 10
-    if DBUOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $TOKEN"  http://127.0.0.1:8000/api/v0/mariauser/read/$DBUSERID` ;then
+    if DBUOKJSON=`curl -s --fail --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN"  https://my.opalstack.com/api/v0/mariauser/read/$DBUSERID` ;then
          printf $CGREEN2
          echo 'DBUser OK lookup.'
          printf $CEND
@@ -126,9 +125,16 @@ else
     fi;
     done
 
-    # curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    # Path to your WordPress installs
-    # SITE_PATH="/home/$USER/apps/"  # need to add app path TODO
+    /bin/mkdir -p $HOME/bin/
+    /bin/wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O $HOME/bin/wp
+    /bin/chmod +x $HOME/bin/wp
+    /$HOME/bin/wp cli update
+    cd $HOME/apps/$APPNAME/
+    $HOME/bin/wp core download
+    $HOME/wp core config --dbhost=localhost --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPWD
+    chmod 644 wp-config.php
+    
+    #$HOME/wp core install --url=yourwebsite.com --title="Your Blog Title" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=you@example.com
 
 fi;
 
