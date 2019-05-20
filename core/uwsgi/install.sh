6bc61b9bd3344303cd2795462d9156f9b0385f42
John Spounias <d3cline@pyl0n.com>
2019-05-20 00:00:40 -0600

    path fix

--- core/uwsgi/install.sh
+++ core/uwsgi/install.sh
@@ -90,4 +90,4 @@ b3NlCnByaW50KGYnV3JvdGUge215YXBwX3dzZ2lfcGF0aH0nKQo=" | base64 --decode > /home/
 chmod +x /home/$USER/apps/$APPNAME/keepalive
 chmod +x /home/$USER/apps/$APPNAME/kill
 
-echo "$(echo '*/1 * * * * /home/$USER/apps/$APPNAME/keepalive' ; crontab -l)" | crontab -
+echo "$(echo '*/1 * * * * /home/'$USER/apps/'$APPNAME'/keepalive' ; crontab -l)" | crontab -
