5339217d3f6f744450839d8ddcbcb2573926dc31
Sean Fulmer <104225+defulmere@users.noreply.github.com>
2019-06-17 14:55:18 +0000

    added env to run_command

--- core/django/install.py
+++ core/django/install.py
@@ -147,10 +147,6 @@ def main():
     appinfo = api.get(f'/app/read/{args.app_uuid}')
     appdir = f'/home/{appinfo["app_user"]}/apps/{appinfo["name"]}'
 
-    # debugging
-    doit = run_command('env')
-    logging.info(doit)
-
     # create tmp dir
     os.mkdir(f'{appdir}/tmp', 0o700)
     logging.info(f'Created directory {appdir}/tmp')
