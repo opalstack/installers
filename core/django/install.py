382d2fb566fcc411c7f50cb0b30d1bb88f7f4c94
Sean Fulmer <104225+defulmere@users.noreply.github.com>
2019-06-17 14:48:22 +0000

    debug

--- core/django/install.py
+++ core/django/install.py
@@ -101,7 +101,7 @@ def run_command(cmd, env=CMD_ENV):
     """runs a command, returns output"""
     logging.info(f'Running: {cmd}')
     try:
-        result = subprocess.check_output(shlex.split(cmd))
+        result = subprocess.check_output(shlex.split(cmd), env=env)
     except subprocess.CalledProcessError as e:
         logging.debug(e.output)
     return result
