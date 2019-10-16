9dde1e04d52f32310158ac5bffed840a01e58926
Sean Fulmer <104225+defulmere@users.noreply.github.com>
2019-10-16 12:47:53 -0600

    run redis from its own app dir

--- community/discourse/install_discourse.sh
+++ community/discourse/install_discourse.sh
@@ -2,7 +2,8 @@
 
 # change the following to your values
 SHELL_USER=your_shell_user
-APP_NAME=discourse_app
+DISCOURSE_APP_NAME=discourse_app
+REDIS_APP_NAME=discourse_redis
 SITE_DOMAIN=domain.com
 DB_NAME=your_db
 DB_USER=your_db_user
@@ -29,7 +30,7 @@ make
 cp bin/brotli ~/bin/
 
 # install redis
-cd ~/apps/$APP_NAME
+cd ~/apps/$REDIS_APP_NAME
 mkdir tmp
 wget http://download.redis.io/redis-stable.tar.gz
 tar zxf redis-stable.tar.gz
@@ -41,15 +42,15 @@ make
 sed -i -e 's/port 6379/port '"$REDIS_PORT"'/' redis.conf
 
 # change username and app name in next 4 lines to your shell user and discourse app name
-sed -i -e 's/^pidfile \/var\/run\/redis_6379.pid/pidfile \/home\/'"$SHELL_USER"'\/apps\/'"$APP_NAME"'\/tmp\/redis.pid/' redis.conf
-sed -i -e 's/^logfile ""/logfile \/home\/'"$SHELL_USER"'\/logs\/'"$APP_NAME"'\/redis.log/' redis.conf
+sed -i -e 's/^pidfile \/var\/run\/redis_6379.pid/pidfile \/home\/'"$SHELL_USER"'\/apps\/'"$REDIS_APP_NAME"'\/tmp\/redis.pid/' redis.conf
+sed -i -e 's/^logfile ""/logfile \/home\/'"$SHELL_USER"'\/logs\/'"$REDIS_APP_NAME"'\/redis.log/' redis.conf
 sed -i -e 's/daemonize no/daemonize yes/' redis.conf
 
 # start redis
-/home/$SHELL_USER/apps/$APP_NAME/redis-stable/src/redis-server /home/$SHELL_USER/apps/$APP_NAME/redis-stable/redis.conf 
+/home/$SHELL_USER/apps/$REDIS_APP_NAME/redis-stable/src/redis-server /home/$SHELL_USER/apps/$REDIS_APP_NAME/redis-stable/redis.conf 
 
 # install discourse
-cd /home/$SHELL_USER/apps/$APP_NAME
+cd /home/$SHELL_USER/apps/$DISCOURSE_APP_NAME
 git clone https://github.com/discourse/discourse.git
 cd discourse
 mkdir -p tmp/pids
@@ -70,7 +71,7 @@ EOF
 
 # configure config/puma.rb
 sed -ie 's/unix.*sock/tcp:\/\/127.0.0.1:'"$DISCOURSE_PORT"'/' config/puma.rb
-sed -ie 's/discourse\/discourse/'"$SHELL_USER"'\/apps\/'"$APP_NAME"'\/discourse/' config/puma.rb
+sed -ie 's/discourse\/discourse/'"$SHELL_USER"'\/apps\/'"$DISCOURSE_APP_NAME"'\/discourse/' config/puma.rb
 
 # prep the instance
 RAILS_ENV=production bundle exec rake db:migrate
@@ -81,7 +82,7 @@ echo "Almost done! Please follow the prompts to create your initial admin user:"
 RAILS_ENV=production bundle exec rake admin:create
 
 # start sidekiq
-RAILS_ENV=production bundle exec sidekiq -d -L $HOME/logs/$APP_NAME/sidekiq.log -P $HOME/apps/$APP_NAME/discourse/tmp/pids/sidekiq.pid
+RAILS_ENV=production bundle exec sidekiq -d -L $HOME/logs/$DISCOURSE_APP_NAME/sidekiq.log -P $HOME/apps/$DISCOURSE_APP_NAME/discourse/tmp/pids/sidekiq.pid
 # start puma & discourse
 RAILS_ENV=production bundle exec pumactl start
 
