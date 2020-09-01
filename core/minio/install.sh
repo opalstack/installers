0b66e388fd9583900d11fc680d95eba84bf44e3d
John Spounias <d3cline@pyl0n.com>
2020-09-01 07:37:44 -0600

    mino started, still needs manage scripts.

---
+++ core/minio/install.sh
@@ -0,0 +1,14 @@
+# Install minio
+echo 'Mino server'
+/bin/mkdir -p  $HOME/apps/$APPNAME/bin/minio
+/bin/wget https://dl.min.io/server/minio/release/linux-amd64/minio -O  $HOME/apps/$APPNAME/bin/minio
+/bin/chmod +x $HOME/apps/$APPNAME/bin/minio
+
+MINIOPASS=`/usr/bin/openssl rand -base64 32`
+
+MINIO_ACCESS_KEY=$USER MINIO_SECRET_KEY=$MINIOPASS $HOME/apps/$APPNAME/bin/minio server $HOME/apps/$APPNAME/mnt/data
+
+wget https://dl.min.io/client/mc/release/linux-amd64/mc -O $HOME/apps/$APPNAME/bin/mc
+/bin/chmod +x $HOME/apps/$APPNAME/bin/mc
+
+/usr/bin/curl -s -X POST --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN" -d'{"id": "'"$UUID"'", "init_created":true, "note":"'"Admin user: $USER / $MINIOPASS"'"}' $API_URL/api/v0/app/init_created/
