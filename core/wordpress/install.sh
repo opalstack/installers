0fa4cac55dd61777d02308bc26deb967df055a74
John Spounias <d3cline@pyl0n.com>
2021-05-31 23:09:55 -0600

    wordpress dont need to escape that line

--- core/wordpress/install.sh
+++ core/wordpress/install.sh
@@ -185,6 +185,6 @@ else
     firstLine=`echo "${coreinstall}" | head -1`
     echo $firstLine
     # Send JSON installed OK.
-    /usr/bin/curl -s -X POST --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN" -d'{"id": \"'$UUID'\"}' $API_URL/api/v1/app/installed/
+    /usr/bin/curl -s -X POST --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN" -d'{"id": "'$UUID'"}' $API_URL/api/v1/app/installed/
 
 fi;
