fa792cbcfd15b71579f3b3de8cb60f5af368a3bc
John Spounias <d3cline@pyl0n.com>
2021-06-01 20:08:48 -0600

    gitea minior version update, new link

--- core/gitea/install.py
+++ core/gitea/install.py
@@ -15,7 +15,7 @@ from urllib.parse import urlparse
 
 API_HOST = os.environ.get('API_URL').strip('https://').strip('http://')
 API_BASE_URI = '/api/v1'
-GITEA_URL = 'https://dl.gitea.io/gitea/1.8/gitea-1.8-linux-amd64'
+GITEA_URL = 'https://dl.gitea.io/gitea/1.14.2/gitea-1.14.2-linux-amd64'
 CMD_ENV = {'PATH': '/usr/local/bin:/usr/bin:/bin','UMASK': '0002',}
 
 
