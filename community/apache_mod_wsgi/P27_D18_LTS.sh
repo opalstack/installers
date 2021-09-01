0a8040eea985d4a2171d09bfb3a7712105332bb6
John Spounias <d3cline@pyl0n.com>
2021-09-01 17:10:56 -0600

    with config

--- community/apache_mod_wsgi/P27_D18_LTS.sh
+++ community/apache_mod_wsgi/P27_D18_LTS.sh
@@ -63,6 +63,67 @@ cd $APPROOT/src/httpd-2.4.41 && make --directory=$APPROOT/src/httpd-2.4.41 insta
 export PYTHONPATH=$APPROOT/lib/python2.7/site-packages
 /bin/easy_install-2.7 --prefix=$APPROOT https://github.com/opalstack/installers/raw/master/community/apache_mod_wsgi/Django-1.8.19.tar.gz
 " > $APPROOT/build.sh
+/bin/chmod +x $APPROOT/build.sh
+
+echo "
+export PATH=\$HOME/.local/bin:\$PATH
+pip2.7 install --user -U pip==20.3.4
+pip2.7 install --user  virtualenv
+" > $APPROOT/pip27.sh
+/bin/chmod +x $APPROOT/pip27.sh
+
+
+echo "
+Define OPAL_USER ${USER}
+Define APP_NAME ${APPNAME}
+Define APP_PORT ${PORT}
+Define PROJ_NAME myproject
+Define APP_ROOT /home/\${OPAL_USER}/apps/\${APP_NAME}
+Define VIRT_ENV \${APP_ROOT}/env
+Define PROJ_ROOT \${APP_ROOT}/\${PROJ_NAME}
+Define LOG_ROOT /home/\${OPAL_USER}/logs/apps/\${APP_NAME}
+
+ServerRoot \${APP_ROOT}/apache2
+
+LoadModule mpm_worker_module modules/mod_mpm_worker.so
+LoadModule authz_core_module modules/mod_authz_core.so
+LoadModule dir_module        modules/mod_dir.so
+LoadModule env_module        modules/mod_env.so
+LoadModule log_config_module modules/mod_log_config.so
+LoadModule mime_module       modules/mod_mime.so
+LoadModule rewrite_module    modules/mod_rewrite.so
+LoadModule setenvif_module   modules/mod_setenvif.so
+LoadModule wsgi_module       modules/mod_wsgi.so
+LoadModule unixd_module      modules/mod_unixd.so
+
+LogFormat \"%{X-Forwarded-For}i %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"\" combined
+CustomLog \${LOG_ROOT}/access_\${APP_NAME}.log combined
+ErrorLog \${LOG_ROOT}/error_\${APP_NAME}.log
+
+DirectoryIndex index.py
+DocumentRoot \${APP_ROOT}/apache2/htdocs
+
+Listen \${APP_PORT}
+KeepAlive Off
+SetEnvIf X-Forwarded-SSL on HTTPS=1
+ServerLimit 1
+StartServers 1
+MaxRequestWorkers 5
+MinSpareThreads 1
+MaxSpareThreads 3
+ThreadsPerChild 5
+
+# python-home = path to your virtualenv
+# python-path = path to your project directory
+# this is usually all of the python path config that you need.
+WSGIDaemonProcess \${APP_NAME} processes=2 threads=12 python-home=\${VIRT_ENV} python-path=\${PROJ_ROOT}
+WSGIProcessGroup \${APP_NAME}
+WSGIRestrictEmbedded On
+WSGILazyInitialization On
+WSGIScriptAlias / \${PROJ_ROOT}/\${PROJ_NAME}/wsgi.py
+" > $APPROOT/apache2/conf/httpd.conf
+
+
 
 # add installed OK
 /usr/bin/curl -s -X POST --header "Content-Type:application/json" --header "Authorization: Token $OPAL_TOKEN" -d'[{"id": "'$UUID'"}]' $API_URL/api/v1/app/installed/
