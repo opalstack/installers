cc692bae7b947b9b3ca55acad8843a6c4599dff3
John Spounias <d3cline@pyl0n.com>
2021-09-02 10:07:15 -0600

    first pip has to be native, second has to init virtualenv

--- community/apache_mod_wsgi/P27_D18_LTS.sh
+++ community/apache_mod_wsgi/P27_D18_LTS.sh
@@ -63,7 +63,7 @@ cd $APPROOT/src/mod_wsgi-4.7.0 && make --directory=$APPROOT/src/mod_wsgi-4.7.0 i
 
 echo "#! /bin/bash
 export PATH=\$HOME/.local/bin:\$PATH
-~/.local/bin/pip2.7 install --user -U pip==20.3.4
+pip2.7 install --user -U pip==20.3.4
 ~/.local/bin/pip2.7 install --user  virtualenv
 ~/.local/bin/virtualenv venv
 source $APPROOT/venv/bin/activate
