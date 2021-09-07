50b0cd99791f2de093976335aed16aa8c75447ad
John Spounias <d3cline@pyl0n.com>
2021-09-06 22:12:41 -0600

    build script for wp2static plugin

---
+++ community/wp2static/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+#exit $RESULT
+
+#!/bin/bash
+
+mkdir -p ~/.local/
+mkdir -p ~/.local/bin
+cp ~/composer.phar ~/.local/bin/composer
+
+git clone https://github.com/leonstafford/wp2static.git
+cd wp2static
+composer install
+composer build wp2static
