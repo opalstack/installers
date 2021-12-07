304e860cd501f5df9b12ca1ca60b2f88f0310976
Sean Fulmer <104225+defulmere@users.noreply.github.com>
2021-12-07 11:31:07 -0700

    change to correct dir

--- core/django/build_python.sh
+++ core/django/build_python.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 PYDIR=$1
-export PATH=$PYDIR/bin:$PATH
-export CPPFLAGS="-I$HOME/apps/testdjango/python/include $CPPFLAGS"
 
 mkdir -p $PYDIR/src
-cd $PYDIR/src
 
 # openssl
-echo "building openssl 1.1.1..."
+cd $PYDIR/src
+echo "building openssl..."
 wget https://www.openssl.org/source/openssl-1.1.1l.tar.gz
 tar zxf openssl-1.1.1l.tar.gz
 cd openssl-1.1.1l
 ./config --prefix=$PYDIR > /dev/null
 make > /dev/null
 make install > /dev/null
-echo "building openssl 1.1.1..."
+echo "finished building openssl."
 
 # python
+echo "building python..."
+export PATH=$PYDIR/bin:$PATH
+export CPPFLAGS="-I$HOME/apps/testdjango/python/include $CPPFLAGS"
+cd $PYDIR/src
 wget https://www.python.org/ftp/python/3.10.1/Python-3.10.1.tar.xz
 tar xf Python-3.10.1.tar.xz
 cd Python-3.10.1
 ./configure --prefix=$PYDIR > /dev/null
 make > /dev/null
 make install > /dev/null
+echo "finished building python."
