335a559e16088eb8a7ccecd19741e725a8ac7c92
John Spounias <john@d3cline.net>
2023-08-14 11:46:27 -0600

    laravel bones

---
+++ core/laravel/install.py
@@ -0,0 +1,186 @@
+#! /usr/bin/python3.6
+
+import argparse
+import sys
+import logging
+import os
+import http.client
+import json
+import textwrap
+import secrets
+import string
+import subprocess
+import shlex
+import random
+from urllib.parse import urlparse
+import re
+
+API_HOST = os.environ.get('API_URL').strip('https://').strip('http://')
+API_BASE_URI = '/api/v1'
+CMD_ENV = {'PATH': '/usr/local/bin:/usr/bin:/bin','UMASK': '0002',}
+LTS_NODE_URL = 'https://nodejs.org/dist/v16.16.0/node-v16.16.0-linux-x64.tar.xz'
+COMPOSER_URL = 'https://getcomposer.org/installer'
+
+class OpalstackAPITool():
+    """simple wrapper for http.client get and post"""
+    def __init__(self, host, base_uri, authtoken, user, password):
+        self.host = host
+        self.base_uri = base_uri
+
+        # if there is no auth token, then try to log in with provided credentials
+        if not authtoken:
+            endpoint = self.base_uri + '/login/'
+            payload = json.dumps({
+                'username': user,
+                'password': password
+            })
+            conn = http.client.HTTPSConnection(self.host)
+            conn.request('POST', endpoint, payload,
+                         headers={'Content-type': 'application/json'})
+            result = json.loads(conn.getresponse().read())
+            if not result.get('token'):
+                logging.warn('Invalid username or password and no auth token provided, exiting.')
+                sys.exit()
+            else:
+                authtoken = result['token']
+
+        self.headers = {
+            'Content-type': 'application/json',
+            'Authorization': f'Token {authtoken}'
+        }
+
+    def get(self, endpoint):
+        """GETs an API endpoint"""
+        endpoint = self.base_uri + endpoint
+        conn = http.client.HTTPSConnection(self.host)
+        conn.request('GET', endpoint, headers=self.headers)
+        return json.loads(conn.getresponse().read())
+
+    def post(self, endpoint, payload):
+        """POSTs data to an API endpoint"""
+        endpoint = self.base_uri + endpoint
+        conn = http.client.HTTPSConnection(self.host)
+        conn.request('POST', endpoint, payload, headers=self.headers)
+        connread = conn.getresponse().read()
+        print(connread)
+        return json.loads(connread)
+
+def create_file(path, contents, writemode='w', perms=0o600):
+    """make a file, perms are passed as octal"""
+    with open(path, writemode) as f:
+        f.write(contents)
+    os.chmod(path, perms)
+    logging.info(f'Created file {path} with permissions {oct(perms)}')
+
+def download(url, localfile, writemode='wb', perms=0o600):
+    """save a remote file, perms are passed as octal"""
+    logging.info(f'Downloading {url} as {localfile} with permissions {oct(perms)}')
+    u = urlparse(url)
+    if u.scheme == 'http':
+        conn = http.client.HTTPConnection(u.netloc)
+    else:
+        conn = http.client.HTTPSConnection(u.netloc)
+    conn.request('GET', u.path)
+    r = conn.getresponse()
+    with open(localfile, writemode) as f:
+        while True:
+            data = r.read(4096)
+            if data:
+                f.write(data)
+            else:
+                break
+    os.chmod(localfile, perms)
+    logging.info(f'Downloaded {url} as {localfile} with permissions {oct(perms)}')
+
+def gen_password(length=20):
+    """makes a random password"""
+    chars = string.ascii_letters + string.digits
+    return ''.join(secrets.choice(chars) for i in range(length))
+
+def run_command(cmd, cwd=None, env=CMD_ENV):
+    """runs a command, returns output"""
+    logging.info(f'Running: {cmd}')
+    try:
+        result = subprocess.check_output(shlex.split(cmd), cwd=cwd, env=env)
+    except subprocess.CalledProcessError as e:
+        logging.debug(e.output)
+    return result
+
+def add_cronjob(cronjob):
+    """appends a cron job to the user's crontab"""
+    homedir = os.path.expanduser('~')
+    tmpname = f'{homedir}/.tmp{gen_password()}'
+    tmp = open(tmpname, 'w')
+    subprocess.run('crontab -l'.split(),stdout=tmp)
+    tmp.write(f'{cronjob}\n')
+    tmp.close()
+    cmd = f'crontab {tmpname}'
+    doit = run_command(cmd)
+    cmd = run_command(f'rm -f {tmpname}')
+    logging.info(f'Added cron job: {cronjob}')
+
+
+
+def main():
+    """run it"""
+    # grab args from cmd or env
+    parser = argparse.ArgumentParser(
+        description='Installs Node.js web app on Opalstack account')
+    parser.add_argument('-i', dest='app_uuid', help='UUID of the base app',
+                        default=os.environ.get('UUID'))
+    parser.add_argument('-n', dest='app_name', help='name of the base app',
+                        default=os.environ.get('APPNAME'))
+    parser.add_argument('-t', dest='opal_token', help='API auth token',
+                        default=os.environ.get('OPAL_TOKEN'))
+    parser.add_argument('-u', dest='opal_user', help='Opalstack account name',
+                        default=os.environ.get('OPAL_USER'))
+    parser.add_argument('-p', dest='opal_password', help='Opalstack account password',
+                        default=os.environ.get('OPAL_PASS'))
+    args = parser.parse_args()
+
+    # init logging
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)s] %(levelname)s: %(message)s')
+    # go!
+    logging.info(f'Started installation of Laravel app {args.app_name}')
+    api = OpalstackAPITool(API_HOST, API_BASE_URI, args.opal_token, args.opal_user, args.opal_password)
+    appinfo = api.get(f'/app/read/{args.app_uuid}')
+    appdir = f'/home/{appinfo["osuser_name"]}/apps/{appinfo["name"]}'
+
+    # get current LTS nodejs
+    cmd = f'mkdir -p {appdir}/node'
+    doit = run_command(cmd)
+    download(LTS_NODE_URL, f'{appdir}/node.tar.xz')
+    cmd = f'tar xf {appdir}/node.tar.xz --strip 1'
+    doit = run_command(cmd, cwd=f'{appdir}/node')
+    CMD_ENV['PATH'] = f'{appdir}/node/bin:{CMD_ENV["PATH"]}'
+
+    # install composer
+    run_command(f'mkdir -p {appdir}/bin')
+    download(LTS_NODE_URL, f'{appdir}/bin/composer-setup.php')
+    run_command( f'/bin/php82 {appdir}/bin/composer-setup.php --install-dir={appdir}/bin --filename=composer')
+
+
+    pattern = r'^#!/usr/bin/env php$'
+    replacement = '#!/usr/bin/env php82'
+    file_path = f'{appdir}/bin/composer'
+
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    new_content = re.sub(pattern, replacement, content, flags=re.MULTILINE)
+
+    with open(file_path, 'w') as file:
+        file.write(new_content)
+
+
+    # finished, push a notice
+    msg = f'See README in app directory for more info.'
+    payload = json.dumps([{'id': args.app_uuid}])
+    finished=api.post('/app/installed/', payload)
+
+    logging.info(f'Completed installation of Node.js app {args.app_name}')
+
+
+if __name__ == '__main__':
+    main()
